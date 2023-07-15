7969d4855f5c918c3425d728379c4cfeb81223a7
MranDvX <franmavazq@gmail.com>
2023-07-15 19:22:10 -0400

    Add file path in vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@GiftSearchApp": `${__dirname}/src/components/GiftSearchApp`,
+    },
+  },
+});
