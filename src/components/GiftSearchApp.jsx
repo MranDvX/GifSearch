06caa833d02e4167c4694bd9ba984e46d3777635
Francisco Mancuello Vazquez <franmavazq@gmail.com>
2024-06-23 03:55:57 -0400

    feat: :sparkles: add list categories

--- src/components/GiftSearchApp.jsx
+++ src/components/GiftSearchApp.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 export const GiftSearchApp = () => {
+  const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
+
   return (
     <>
       <h1>Gift Search</h1>
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
     </>
-  )
-}
+  );
+};
