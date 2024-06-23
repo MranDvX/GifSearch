f596cbfe8e756cb9b449fa8a56c43670c86910e4
Francisco Mancuello Vazquez <franmavazq@gmail.com>
2024-06-23 03:42:42 -0400

    feat: :lipstick: add styles css

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { GiftSearchApp } from './components/GiftSearchApp'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { GiftSearchApp } from "./components/GiftSearchApp";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GiftSearchApp />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
