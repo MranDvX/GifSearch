8323e95e4aa4fbc9b493e6c5f0eb92e3be72d327
Francisco Mancuello Vazquez <franmavazq@gmail.com>
2024-06-23 04:20:46 -0400

    feat: :sparkles: add new category

--- src/components/GiftSearchApp.jsx
+++ src/components/GiftSearchApp.jsx
@@ -3,9 +3,14 @@ import { useState } from "react";
 export const GiftSearchApp = () => {
   const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
 
+  const addCategory = () => {
+    setCategories(["Naruto", ...categories]);
+  };
+
   return (
     <>
       <h1>Gift Search</h1>
+      <button onClick={addCategory}>Agregar</button>
       <ol>
         {categories.map((category) => {
           return <li key={category}>{category}</li>;
